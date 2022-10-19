ee2ef11fd8b1ff31efdc2551f38bf6dc850644d2
Andy Jesus <xandyx2014@gmail.com>
2022-10-19 10:22:01 -0400

    add  new changes

--- Complete/EFCorePeliculas/Entidades/Conversiones/MonedaASimboloConverter.cs
+++ Complete/EFCorePeliculas/Entidades/Conversiones/MonedaASimboloConverter.cs
@@ -2,17 +2,17 @@
 
 namespace EFCorePeliculas.Entidades.Conversiones
 {
-    public class MonedaASimboloConverter: ValueConverter<Moneda, string>
+    public class MonedaASimboloConverter : ValueConverter<Moneda, string>
     {
         public MonedaASimboloConverter()
-            :base(
+            : base(
                     valor => MapeoMonedaString(valor),
                     valor => MapeoStringMoneda(valor)
                  )
         {
 
         }
-
+        // para guardar
         private static string MapeoMonedaString(Moneda valor)
         {
             return valor switch
@@ -23,7 +23,7 @@ namespace EFCorePeliculas.Entidades.Conversiones
                 _ => ""
             };
         }
-
+        // para obtener
         private static Moneda MapeoStringMoneda(string valor)
         {
             return valor switch
